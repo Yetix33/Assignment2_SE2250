321400dd95739ca3f0d330de9bbd0d3538af51c2
Yetix33 <apate388@uwo.ca>
2018-02-04 02:36:59 -0500

    initial commit
    points system is done using tags

---
+++ Assets/Main.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+	public GameObject PickUp;
+	private GameObject itemA;
+	private GameObject itemB;
+	private GameObject itemC;
+	private GameObject itemD;
+	private GameObject itemE;
+	private GameObject itemF;
+	private GameObject itemG;
+	private GameObject itemH;
+	public Material materialA;
+	public Material materialB;
+	public Material materialC;
+	public Material materialD;
+	public Material materialE;
+	public Material materialF;
+	public Material materialG;
+	public Material materialH;
+
+	public Color silver = new Color(128.0f, 128.0f, 128.0f);
+	public Color bronze = new Color(205.0f,127.0f,50.0f);
+
+	// Use this for initialization
+	void Start () {
+		itemA = Instantiate (PickUp);
+		itemB = Instantiate (PickUp);
+		itemC = Instantiate (PickUp);
+		itemD = Instantiate (PickUp);
+		itemE = Instantiate (PickUp);
+		itemF = Instantiate (PickUp);
+		itemG = Instantiate (PickUp);
+		itemH = Instantiate (PickUp);
+		materialA = itemA.GetComponent<Renderer> ().material;
+		materialA.color = silver;
+		itemA.tag = "silver";
+		materialB = itemB.GetComponent<Renderer> ().material;
+		materialB.color = bronze;
+		itemB.tag = "bronze";
+		materialC = itemC.GetComponent<Renderer> ().material;
+		materialC.color = silver;
+		itemC.tag = "silver";
+		materialD = itemD.GetComponent<Renderer> ().material;
+		materialD.color = bronze;
+		itemD.tag = "bronze";
+		materialE = itemE.GetComponent<Renderer> ().material;
+		materialE.color = silver;
+		itemE.tag = "silver";
+		materialF = itemF.GetComponent<Renderer> ().material;
+		materialF.color = bronze;
+		itemF.tag = "bronze";
+		/*materialG = itemG.GetComponent<Renderer> ().material;
+		materialG.color = bronze;
+		materialH = itemH.GetComponent<Renderer> ().material;
+		materialH.color = bronze;
+*/
+		itemA.transform.position = new Vector3 (5.5f,0.5f, 5.5f);
+		itemB.transform.position = new Vector3 (-5.5f,0.5f, 5.5f);
+		itemC.transform.position = new Vector3 (5.5f,0.5f, -5.5f);
+		itemD.transform.position = new Vector3 (-5.5f,0.5f, -5.5f);
+		itemE.transform.position = new Vector3 (0.0f,0.5f, 5.5f);
+		itemF.transform.position = new Vector3 (5.5f,0.5f, 0.0f);
+		itemG.transform.position = new Vector3 (0.0f,0.5f, -5.5f);
+		itemH.transform.position = new Vector3 (-5.5f,0.5f, 0.0f);
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+
+public class PlayerController : MonoBehaviour {
+	// Update is called once per frame
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+	public Text countText;
+	//public Main maincontroller;
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
+		//maincontroller = GetComponent<Main> ();
+
+	}
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+
+
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 roll = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.AddForce (roll*speed);
+
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		print("ok");
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count += 5;
+		} else if (other.gameObject.CompareTag ("silver")) {
+			other.gameObject.SetActive (false);
+			count += 3;
+			
+		} else {
+			other.gameObject.SetActive (false);
+			count += 1;
+		
+		}
+
+		countText.text = "Count: " + count.ToString ();
+			
+
+
+
+		}
+	
+	
+	}
+
+
+
